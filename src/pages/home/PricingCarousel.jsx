4676e536d04a72cb33cf342c69788ab5fd0d95e2
chrismiWeb <chrismibiteso@gmail.com>
2025-08-19 14:10:29 +0100

    added descripton

--- src/pages/home/PricingCarousel.jsx
+++ src/pages/home/PricingCarousel.jsx
@@ -9,52 +9,62 @@ const PricingCarousel = () => {
     {
       title: "BOOKING DEPOSIT",
       price: "$30",
+      description: "(non-refundable to guarantee your spot)",
       image: "/images/8d276812-95f5-40cb-90c7-5b924f426960.JPG", // replace with your actual image URL
     },
-    {
+      {
       title: "MINI SESSION",
       price: "$80",
+      description: "(20-30 min, 8-12 edited photos)",
       image: "/images/selective-focus-dslr-camera-body.jpg", // replace with your actual image URL
     },
     {
       title: "SMALL EVENT",
       price: "$75/hr",
+      description: "(birthdays, showers, etc.)",
       image: "/images/medium-shot-woman-bridal-party.jpg",
     },
     {
       title: "GRADUATIONS",
       price: "$100-$140",
+      description: "(Cap & Gown/session)",
       image: "/images/front-view-graduation-straws-fancy-glass.jpg",
     },
     {
       title: "PORTRAITS",
       price: "$110/hr",
+      description: "(individual/couples)",
       image: "/images/young-beautiful-woman-posing-wrapping-up-grey-cloth.jpg",
     },
 
     {
       title: "SPORTS/ACTIVITIES",
       price: "$110",
+      description: "(1 hr, 20-25 edited photos)",
       image: "/images/a95b9a6f-e5ef-4cb8-afcf-efb2e1c80c2a.JPG",
     },
     {
       title: "FAMILY SESSION",
       price: "$150/hr",
+      description: "(Family Photography Session)",
       image: "/images/family-three-spending-time-together-outdoors-father-s-day.jpg",
     },
     {
       title: "EXTENDED SESSION",
       price: "$200",
+      description: "(1-2 hrs, 35-50 edited photos)",
       image: "/images/192d168a-2996-460c-a3d2-bda73d63051a.JPG",
     },
     {
       title: "HALF DAY EVENT ",
       price: "$300",
+      description: "(3-4 hrs, 75-100 edited photos)",
       image: "/images/10624.jpg",
     },
     {
       title: "FULL DAY EVENT ",
-      price: "$500",
+      price: "$550",
+      description: "(6-8 hrs, 150-200 edited photos)",
       image: "/images/decorated-banquet-hall-with-flowers.jpg",
     },
   ];
@@ -66,7 +76,7 @@ const PricingCarousel = () => {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2500,
+    autoplaySpeed: 2000,
     arrows: false,
     responsive: [
       {
@@ -91,7 +101,7 @@ const PricingCarousel = () => {
     slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2500,
+    autoplaySpeed: 2000,
     arrows: false,
     responsive: [
       {
@@ -116,7 +126,7 @@ const PricingCarousel = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2500,
+    autoplaySpeed: 2000,
     arrows: false,
     responsive: [
       {
@@ -151,13 +161,15 @@ const PricingCarousel = () => {
             >
               <div className="absolute inset-0 bg-black opacity-60 z-0"></div>
               <div className="relative z-10">
-                <h3 className="md:text-[20px] lg:text-lg font-semibold text-white tracking-widest uppercase">
+                <h3 className="md:text-[20px] lg:text-lg text-[22px] font-semibold text-white tracking-widest uppercase">
                     {session.title}
                 </h3>
-                <h1 className="md:text-[35px] lg:text-6xl font-bold mt-2 text-white">{session.price}</h1>
-                
+                <h1 className="md:text-[35px] lg:text-6xl text-[30px] font-bold mt-2 text-white">{session.price}</h1>
+                <h3 className=" md:text-[20px] text-[20px] lg:text-lg text-white tracking-widest uppercase mt-[10px] font-bold">
+                    {session.description}
+                </h3>
                 <a href="/booking">
-                <button className="bg-[#b8894d] cursor-pointer text-white px-6 py-2 mt-4 rounded hover:bg-[#a67c3d] transition">
+                <button className="bg-[#b8894d] font-bold text-white px-6 py-2 mt-4 rounded cursor-pointer hover:bg-[#a67c3d] transition">
                     BOOK NOW
                 </button>
                 </a>
